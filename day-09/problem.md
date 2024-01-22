a841d87fe6168bfaef12b0688e8581313e811c23
Nigel Wilson <nigel@stemgarden.org>
2024-01-22 16:22:47 -0600

    Solve day 9 in Java and JS
    Beside the recursion problem this assignment also features challenges with writing output to Files instead of STDOUT

---
+++ day-09/problem.md
@@ -0,0 +1,43 @@
+# Objective
+
+Today, we are learning about an algorithmic concept called recursion. Check out the Tutorial tab for learning materials and an instructional video.
+
+# Recursive Method for Calculating Factorial
+
+$$factorial(N) = N \times factorial(N - 1)$$
+
+# Function Description
+
+Complete the factorial function in the editor below. Be sure to use recursion.
+
+factorial has the following parameter: $n$
+
+* int n: an integer
+
+# Returns
+
+* int: the factorial of $n$
+
+Note: If you fail to use recursion or fail to name your recursive function factorial or Factorial, you will get a score of 0.
+
+# Input Format
+
+A single integer, $n$ (the argument to pass to factorial).
+
+# Constraints
+
+* $2 \leq n \leq 12$
+
+* Your submission must contain a recursive function named factorial.
+
+# Sample Input
+
+```
+3
+```
+
+# Sample Output
+
+```
+6
+```
---
+++ day-09/solution.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'factorial' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function factorial(n) {
+    // Write your code here
+    if (n <= 1) {
+      return n;
+    } 
+    else {
+      return n * factorial(--n);
+    }
+
+}
+
+function main() {
+    const ws = fs.createWriteStream("./output.txt");
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const result = factorial(n);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
